a63784d4a21b6e4e4cb0b7fd41e9c9243b09717f
Muhammad Najwan <najwanhassan03@gmail.com>
2024-07-26 04:59:26 +0800

    insertion sort

--- python/insertion.py
+++ python/insertion.py
@@ -1,13 +1,27 @@
+from random import randint
 from time import time
-init_time = time()
-# raw_data = [10, 14, 28, 11, 7, 16, 30, 50, 25, 18]
-raw_data = [17, 92, 55, 31, 74, 8, 62, 49, 23, 37, 82, 46, 11, 69, 28, 91, 14, 72, 43, 19, 67, 29, 58, 83, 7, 64, 38, 13, 79, 53, 26, 89, 61, 16, 71, 44, 34, 97, 22, 77,73, 59, 14, 82, 61, 46, 38, 97, 23, 55, 74, 49, 65, 31, 44, 8, 86, 19, 68, 7, 88, 26, 71, 5, 34, 91, 53, 29, 56, 37, 79, 22, 64, 47, 1, 94, 16, 43, 77, 11, 49, 82, 38, 32, 72, 11, 94, 61, 24, 86, 53, 29, 43, 16, 34, 19, 77, 5, 82, 2, 55, 37, 83, 8, 28, 26, 69, 59, 14, 76, 1, 64, 47, 71, 49, 23, 58, 46, 97, 7, 88, 44, 22, 94, 65, 41, 74, 13, 79, 17, 92, 55, 31, 74, 8, 62, 49, 23, 37, 82, 46, 11, 69, 28, 91]
 
-def insertion(raw_data):
+raw_data = []
+
+test_data = [7,4,5,2]
+
+def random_generator(target):
+    for i in range(target):
+        raw_data.append(randint(0, target))
+def insertion():
+    target_point = 0
     for i in raw_data:
-        for j in range(len(raw_data) -1):
-            if raw_data[j] > raw_data[j + 1]:
-                raw_data[j], raw_data[j+1] = raw_data[j+1], raw_data[j]
-    return raw_data 
-print(f"Sorted: {insertion(raw_data)}")
-print(f"Time: {time() - init_time}")
+        for j in range(len(raw_data) - 1):
+            # print(f"target point: {target_point} --> {raw_data}")
+            if raw_data[target_point] > raw_data[j+1]:
+                raw_data[target_point], raw_data[j+1] = raw_data[j+1], raw_data[target_point]
+            target_point += 1
+        target_point = 0
+
+random_generator(1000)
+print(f"Raw data: {raw_data}")
+start = time()
+insertion()
+finish = time()
+print(f"Sorted data: {raw_data}")
+print(f"Time: {finish-start}")
