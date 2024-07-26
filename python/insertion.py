654e5cfe48ddca6ae5c7af0e67b0b415b15f99b0
Muhammad Najwan <najwanhassan03@gmail.com>
2024-07-26 18:06:10 +0800

    Insertion sort

--- python/insertion.py
+++ python/insertion.py
@@ -1,3 +1,6 @@
+# Time: O(n^2)
+# Space: O(1)
+
 from random import randint
 from time import time
 
@@ -8,17 +11,17 @@ test_data = [7,4,5,2]
 def random_generator(target):
     for i in range(target):
         raw_data.append(randint(0, target))
-def insertion():
-    target_point = 0
-    for i in raw_data:
-        for j in range(len(raw_data) - 1):
-            # print(f"target point: {target_point} --> {raw_data}")
-            if raw_data[target_point] > raw_data[j+1]:
-                raw_data[target_point], raw_data[j+1] = raw_data[j+1], raw_data[target_point]
-            target_point += 1
-        target_point = 0
 
-random_generator(1000)
+def insertion():
+    for i in range(1, len(raw_data)):
+        j = i
+        while j > 0:
+            print(f"point: {j} --> {raw_data}")
+            if raw_data[j-1] > raw_data[j]:
+                raw_data[j], raw_data[j-1] = raw_data[j-1], raw_data[j]
+            j -= 1
+
+random_generator(10)
 print(f"Raw data: {raw_data}")
 start = time()
 insertion()
