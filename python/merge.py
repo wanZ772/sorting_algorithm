0fb865989351ae5cddc12ddd75dc5e7a3eaf8f83
Muhammad Najwan <najwanhassan03@gmail.com>
2024-07-26 18:54:15 +0800

    Merge sort

--- python/merge.py
+++ python/merge.py
@@ -1,41 +1,50 @@
+# Time: O(n log n)
+# Space: O(log2n)
+
 from time import time
 from random import randint
-# raw_data = [10, 14, 28, 11, 7, 16, 30, 50]
-raw_data = []
-def random_generator(test_limit):
-    global raw_data
-    for i in range(test_limit):
-        raw_data.append(randint(0, test_limit))
 
-def merge(raw_data):
-    
-    if len(raw_data) > 1:
-        mid = len(raw_data) // 2
-        left_list = raw_data[:mid]
-        right_list = raw_data[mid:]
-        merge(left_list)
-        merge(right_list)
-        left, right, interator = 0,0,0
+raw_data = []
+
+def random_generator(target):
+    for i in range(target):
+        raw_data.append(randint(0, target))
+def merge(data):
+    n = len(data)
+
+    if n == 1:
+        return data
+    else:
+        mid = n // 2
+        left = data[:mid]
+        right = data[mid:]
         
-        while left < len(left_list)  and right < len(right_list):
-            if left_list[left] < right_list[right]:
-                raw_data[interator] = left_list[left]
-                left += 1
+        left = merge(left)
+        right = merge(right)
+
+        sorted_data = []
+
+        left_point, right_point, i = 0, 0, 0
+
+        while left_point < len(left) and right_point < len(right):
+            if left[left_point] < right[right_point]:
+                sorted_data.append(left[left_point])
+                left_point += 1
             else:
-                raw_data[interator] = right_list[right]
-                right += 1
-            interator += 1
-        while left < len(left_list):
-            raw_data[interator] = left_list[left]
-            left += 1
-            interator += 1
-        while right < len(right_list):
-            raw_data[interator] = right_list[right]
-            right += 1
-            interator += 1
-random_generator(100000)
-init_time = time()
-merge(raw_data)
-final_time = time() - init_time
-print(raw_data)
-print(f"Time: {final_time}")
+                sorted_data.append(right[right_point])
+                right_point += 1
+        while left_point < len(left):
+            sorted_data.append(left[left_point])
+            left_point += 1
+        while right_point < len(right):
+            sorted_data.append(right[right_point])
+            right_point += 1
+        return sorted_data
+
+random_generator(10)
+print(f"Raw data: {raw_data}")
+start = time()
+sorted_ = merge(raw_data)
+finish = time()
+print(f"Sorted data: {sorted_}")
+print(f"Time: {finish-start}")
