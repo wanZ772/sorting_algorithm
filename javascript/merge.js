943c2f71f569219217efd63f3880db9785558494
Muhammad Najwan <najwanhassan03@gmail.com>
2024-07-28 23:32:06 +0800

    Merge sort

--- javascript/merge.js
+++ javascript/merge.js
@@ -1,59 +1,51 @@
-raw_data = [7,4,2,5,0];
+unsorted = [1,5,7,4,10,8,3,2,0,6];
 
 
-function merge(data)  {
-  n = data.length;
+function divide(array)  {
+  n = array.length;
   
   if (n == 1) {
-    return data;
-    
+    return array;
   } else  {
-    mid = parseInt(n / 2);
-    left = [];
-    right = [];
-    for (i = 0; i < n; i++) {
-      if (i < mid)  {
-        left.push(data[i]);
-      } else  {
-        right.push(data[i]);
-      }
-    }
+    let mid = parseInt(n / 2);
+    let left = array.slice(0, mid);
+    let right = array.slice(mid);
     
+    return merge(divide(left), divide(right));
     
-    merge(right);
-    merge(left);
-    
-    left_point = 0; right_point = 0;
-    
-    left_length = left.length;
-    right_length = right.length;
-    
-    sorted_data = [];
-    
-    while ((left_point < left_length) && (right_point < right_length))  {
-      if (left[left_point] < right[right_point])  {
-        sorted_data.push(left[left_point]);
-        left_point++;
-      } else  {
-        sorted_data.push(right[right_point]);
-        right_point++;
-      }
-    }
-    
-    while (left_point < left_length)  {
-        sorted_data.push(left[left_point]);
-        left_point++;
-    }
-    
-    while (right_point < right_length)  {
-        sorted_data.push(right[right_point]);
-        right_point++;
-    }
-    
-    return sorted_data;
   }
 }
 
-sorted_ = merge(raw_data);
+function merge(left, right) {
+  left_point = 0; right_point = 0;
+  left_length = left.length; right_length = right.length;
+  
+  sorted_ = [];
+  
+  while ((left_point < left_length) && (right_point < right_length))  {
+    if (left[left_point] < right[right_point])  {
+      sorted_.push(left[left_point]);
+      left_point++;
+    } else  {
+      sorted_.push(right[right_point]);
+      right_point++;
+    }
+  }
+  
+  while (left_point < left_length)  {
+    sorted_.push(left[left_point]);
+      left_point++;
+  }
+  
+  while (right_point < right_length)  {
+    sorted_.push(right[right_point]);
+      right_point++;
+  }
+  
+  return sorted_;
+  
+}
 
-console.log("Sorted: " + sorted_);
+console.log("Unsorted array: " + unsorted)
+sorted = divide(unsorted)
+console.log("Sorted array: " + sorted)
